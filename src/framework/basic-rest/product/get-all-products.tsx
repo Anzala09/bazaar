e6279890e729af7248c09153ee33ba7027038cd7
Anzala Islam <anzala.jmi@gmail.com>
2022-03-03 20:58:46 +0530

    Show Color & Size on product click

--- src/framework/basic-rest/product/get-all-products.tsx
+++ src/framework/basic-rest/product/get-all-products.tsx
@@ -3,6 +3,7 @@ import {API_ENDPOINTS, API_ENDPOINTS_V2} from "@framework/utils/api-endpoints";
 import {httpV2} from "@framework/utils/http";
 import {useInfiniteQuery} from "react-query";
 import {getSlug, imageUrlV2} from "@utils/constants";
+import {getVariationsV2} from "@framework/utils/get-variations";
 
 type PaginatedProduct = {
 	data: Product[];
@@ -17,7 +18,7 @@ const fetchProducts = async ({ queryKey }: any) => {
 	const [_key, _params] = queryKey;
 	const qs = require('qs');
 	const query = qs.stringify({
-		populate: 'Image',
+		populate: '*',
 		pagination: {
 			pageSize: 25,
 			// page: 999, // TODO: Enter page number to fetch given page
@@ -34,14 +35,27 @@ const fetchProducts = async ({ queryKey }: any) => {
 		},
 	};
 };
-
+const getAttachment = (imageData) => {
+	return {
+		id: imageData.id,
+		thumbnail: imageUrlV2(imageData.attributes.formats.thumbnail.url),
+		original: imageUrlV2(imageData.attributes.url),
+	};
+};
+const getVariationV1 = (variations) => {
+	return []
+		.concat(variations['size'])
+		.concat(variations['color'])
+		.map((variation) => {
+		variation['attribute'] = variation['attributes'];
+		return variation;
+	});
+}
 const transformProductV2ToV1 = (productsV2: ProductV2[]): Product[] => {
 	return productsV2.map((productV2) => {
-		let attachment: Attachment = {
-			id: productV2.attributes.Image.data.id,
-			thumbnail: imageUrlV2(productV2.attributes.Image.data.attributes.formats.thumbnail.url),
-			original: imageUrlV2(productV2.attributes.Image.data.attributes.url),
-		};
+		let attachment: Attachment = getAttachment(productV2.attributes.Image.data);
+		let variations = getVariationsV2(productV2?.attributes?.sizes?.data, productV2?.attributes?.colors?.data);
+		let oldVariations = getVariationV1(variations);
 		return {
 			id: productV2.id,
 			name: productV2.attributes.Name,
@@ -49,6 +63,8 @@ const transformProductV2ToV1 = (productsV2: ProductV2[]): Product[] => {
 			price: productV2.attributes.Price,
 			sale_price: productV2.attributes.SalePrice,
 			quantity: 0,
+			description: productV2.attributes.Description,
+			variations: oldVariations,
 			image: attachment,
 		}
 	});
